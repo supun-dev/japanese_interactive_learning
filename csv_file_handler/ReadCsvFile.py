a7493f55840e8cf6c24a1133fe334a9b99628583
Supun Amarasinghe <supun.dev91@gmail.com>
2025-01-19 22:05:24 +0900

    Made the search case insensitive

--- csv_file_handler/ReadCsvFile.py
+++ csv_file_handler/ReadCsvFile.py
@@ -16,8 +16,9 @@ def print_csv_file(file_name):
 def search_japanese_word_by_english_meaning(file_name, word):
     reader = open_and_read_csv(file_name)
     list_of_rows = []
+    word_lower = word.lower()
     for row in reader:
-        if word in row:
+        if any(word_lower in cell.lower() for cell in row):
             list_of_rows.append(row)
     if len(list_of_rows) != 0:
         return list_of_rows
