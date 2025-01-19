83fd36a7c940b2917339a44dfe7073a9be2949d7
Supun Amarasinghe <supun.dev91@gmail.com>
2025-01-19 16:59:49 +0900

    minor refactoring

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from csv_file_handler.read_csv_file import search_japanese_word_by_english_meaning
+from csv_file_handler.read_csv_file import view_japanese_word_by_english_meaning
 
 
 def main():
     file_path = "resources\\vocabulary_N5.csv"
-    search_japanese_word_by_english_meaning(file_path,"blue");
+    view_japanese_word_by_english_meaning(file_path,"blue");
 
 if __name__ == "__main__":
     main()
