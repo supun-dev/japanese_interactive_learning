afa549c2e8f1aa820821f6d9812bcd3ddf5dcf61
Supun Amarasinghe <supun.dev91@gmail.com>
2025-01-19 22:06:25 +0900

    Changed HTTP method to GET

--- rest_controller/RestController.py
+++ rest_controller/RestController.py
@@ -5,6 +5,6 @@ from service.RestService import get_records_by_english_word
 api = Flask(__name__)
 
 
-@api.route('/searchWordByEnglishMeaning/<english_word>', methods=['POST'])
+@api.route('/searchWordByEnglishMeaning/<english_word>', methods=['GET'])
 def search_word_by_english_meaning(english_word):
     return jsonify(get_records_by_english_word(english_word))
