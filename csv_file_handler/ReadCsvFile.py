a04d709cb49dd20d195f47d3630e6ac424f52494
Supun Amarasinghe <supun.dev91@gmail.com>
2025-01-23 00:45:16 +0900

    Fixes

--- csv_file_handler/ReadCsvFile.py
+++ csv_file_handler/ReadCsvFile.py
@@ -7,18 +7,18 @@ class ReadCsvFile:
     @staticmethod
     def open_and_read_csv(file_name):
         if not ReadCsvFile._list_of_rows:
-            print("Reading csv file")
             with open(file_name, newline='', encoding='utf-8') as csvfile:
                 # list() loads rows into memory. This is to prevent => ValueError: I/O operation on closed file.
                 ReadCsvFile._list_of_rows = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
         return ReadCsvFile._list_of_rows
 
+    @staticmethod
     def print_csv_file(file_name):
         reader = ReadCsvFile.open_and_read_csv(file_name)
         for row in reader:
             print(row)
 
-
+    @staticmethod
     def search_japanese_word_by_english_meaning(file_name, word):
         reader = ReadCsvFile.open_and_read_csv(file_name)
         list_of_rows = []
@@ -29,7 +29,7 @@ class ReadCsvFile:
         if len(list_of_rows) != 0:
             return list_of_rows
 
-
+    @staticmethod
     def view_japanese_word_by_english_meaning(file_name, word):
         print("kanji : furigana : english")
         for row in ReadCsvFile.search_japanese_word_by_english_meaning(file_name, word):
