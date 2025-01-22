86fd5646bff121e7d5f9a3a1bae47b17164737dd
Supun Amarasinghe <supun.dev91@gmail.com>
2025-01-23 00:39:28 +0900

    static list to store data without reading file for every request

--- csv_file_handler/ReadCsvFile.py
+++ csv_file_handler/ReadCsvFile.py
@@ -1,30 +1,36 @@
 import csv
 
 
-def open_and_read_csv(file_name):
-    with open(file_name, newline='', encoding='utf-8') as csvfile:
-        # list() loads rows into memory. This is to prevent => ValueError: I/O operation on closed file.
-        return list(csv.reader(csvfile, delimiter=',', quotechar='"'))
+class ReadCsvFile:
+    _list_of_rows = []
+
+    @staticmethod
+    def open_and_read_csv(file_name):
+        if not ReadCsvFile._list_of_rows:
+            print("Reading csv file")
+            with open(file_name, newline='', encoding='utf-8') as csvfile:
+                # list() loads rows into memory. This is to prevent => ValueError: I/O operation on closed file.
+                ReadCsvFile._list_of_rows = list(csv.reader(csvfile, delimiter=',', quotechar='"'))
+        return ReadCsvFile._list_of_rows
+
+    def print_csv_file(file_name):
+        reader = ReadCsvFile.open_and_read_csv(file_name)
+        for row in reader:
+            print(row)
 
 
-def print_csv_file(file_name):
-    reader = open_and_read_csv(file_name)
-    for row in reader:
-        print(row)
+    def search_japanese_word_by_english_meaning(file_name, word):
+        reader = ReadCsvFile.open_and_read_csv(file_name)
+        list_of_rows = []
+        word_lower = word.lower()
+        for row in reader:
+            if any(word_lower in cell.lower() for cell in row):
+                list_of_rows.append(row)
+        if len(list_of_rows) != 0:
+            return list_of_rows
 
 
-def search_japanese_word_by_english_meaning(file_name, word):
-    reader = open_and_read_csv(file_name)
-    list_of_rows = []
-    word_lower = word.lower()
-    for row in reader:
-        if any(word_lower in cell.lower() for cell in row):
-            list_of_rows.append(row)
-    if len(list_of_rows) != 0:
-        return list_of_rows
-
-
-def view_japanese_word_by_english_meaning(file_name, word):
-    print("kanji : furigana : english")
-    for row in search_japanese_word_by_english_meaning(file_name, word):
-        print(" : ".join(row))
+    def view_japanese_word_by_english_meaning(file_name, word):
+        print("kanji : furigana : english")
+        for row in ReadCsvFile.search_japanese_word_by_english_meaning(file_name, word):
+            print(" : ".join(row))
--- service/RestService.py
+++ service/RestService.py
@@ -1,10 +1,10 @@
-from csv_file_handler.ReadCsvFile import search_japanese_word_by_english_meaning
+from csv_file_handler.ReadCsvFile import ReadCsvFile
 from modal.DictionaryRecord import DictionaryRecord
 
 
 def get_records_by_english_word(english_word):
     file_path = "resources\\vocabulary_N5.csv"
-    records = search_japanese_word_by_english_meaning(file_path, english_word)
+    records = ReadCsvFile.search_japanese_word_by_english_meaning(file_path, english_word)
     return handle_response_for_english_word(records, english_word)
 
 
